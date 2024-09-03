406df3b9daacd35309304c795fbe72d9ba71eb01
tbhhero <tbhhero84@gmail.com>
2024-09-03 20:57:12 +0530

    Updating

--- LoginAndSignUp/app/src/main/java/com/practice/loginandsignup/SignUp.kt
+++ LoginAndSignUp/app/src/main/java/com/practice/loginandsignup/SignUp.kt
@@ -1,26 +1,20 @@
 package com.practice.loginandsignup
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    fun clk(v: View?){
-        val intent= Intent(this,Login::class.java)
-        startActivity(intent)
     }
 }
