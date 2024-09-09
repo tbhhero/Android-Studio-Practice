9c1db6c3d09fa28489d562b58ec775ad2ac3bb47
tbhhero <tbhhero84@gmail.com>
2024-09-09 10:50:47 +0530

    Adding Kotlin Code in CheckBox

--- CheckBox/app/src/main/java/com/practice/checkbox/MainActivity.kt
+++ CheckBox/app/src/main/java/com/practice/checkbox/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.practice.checkbox
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var chkPizza: CheckBox
+    lateinit var chkBurger: CheckBox
+    lateinit var chkPasta: CheckBox
+    lateinit var btnTotalPrice: Button
+    lateinit var tvTotalPrice: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        chkPizza = findViewById(R.id.chk_pizza)
+        chkBurger = findViewById(R.id.chk_burger)
+        chkPasta = findViewById(R.id.chk_pasta)
+
+        btnTotalPrice = findViewById(R.id.btn_total_price)
+        tvTotalPrice = findViewById(R.id.tv_total_price)
+
+        btnTotalPrice.setOnClickListener {
+            calculateTotal()
         }
+    }
+
+    private fun calculateTotal() {
+        var totalPrice = 0
+
+        if (chkPizza.isChecked) {
+            totalPrice += 250
+
+        }
+        if (chkBurger.isChecked) {
+            totalPrice += 80
+        }
+        if (chkPasta.isChecked) {
+            totalPrice += 90
+        }
+
+        tvTotalPrice.text = "Total: \u20B9"+"$totalPrice"
+
+        Toast.makeText(this, "Total Price: \u20B9"+"$totalPrice", Toast.LENGTH_SHORT).show()
     }
 }
